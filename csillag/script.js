de97ac85d656b07eda9f4c9b09c7ceecc48c76b9
al3xrust <al3xrustgame@gmail.com>
2024-12-17 00:25:12 +0100

    csillag.html

---
+++ csillag/script.js
@@ -0,0 +1,5 @@
+document.getElementById("button").addEventListener("click", function () {
+    // Állítsd be a cél URL-t
+    const url = "https://rickroll.it/rickroll.mp4"; 
+    window.location.href = url; // Navigálás a megadott linkre
+  });
